567a10eb4b7a785058b6326ca5090994987e4c8c
dogcalas <dogcalas@gmail.com>
2026-02-09 00:31:20 +0100

    Adjust left drive speed constant for improved steering control

--- main.ts
+++ main.ts
@@ -61,7 +61,7 @@ enum pwm_led_r {
 //% color="#AA278D" groups=['Motor', 'RGB LED', 'Ultrasonic Sensor', 'Photoresistance Sensor', 'Line Tracking', 'Servo', 'others']
 namespace MiniCar {
     const DRIVE_SPEED = 180
-    const DRIVE_SPEED_LEFT_ADJUST = 25  // Ajusta este valor si deriva a la izquierda (+5 a +20)
+    const DRIVE_SPEED_LEFT_ADJUST = 30  // Ajusta este valor si deriva a la izquierda (+5 a +20)
     const DRIVE_SPEED_RIGHT_ADJUST = 0 // Ajusta este valor si deriva a la derecha
     const ROTATE_SPEED = 140
     const ROTATE_BRAKE_SPEED = 100
